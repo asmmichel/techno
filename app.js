10afe60c45b9642ece56064af7b1c61a9e1968a6
Michel Maciel <michel.asm@hotmail.com>
2023-08-15 12:40:36 -0300

    criando e estilizando cartao aberto

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ new Vue({
   el: "#app",
   data: {
     arrayOBJProdutos: [],
+    dadosProduto: {},
   },
   methods: {
     async fazerFetchGithub() {
       this.arrayOBJProdutos = await (await fetch("./api/produtos.json")).json()
+    },
+    async fazerFetchPastaProdutos(objProdutoId) {
+      this.dadosProduto = await (await fetch(`./api/produtos/${objProdutoId}/dados.json`)).json()
     },
   },
   filters: {
