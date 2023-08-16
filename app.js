787b1c12832378bda3f69b9ac79d94e24d656780
Michel Maciel <michel.asm@hotmail.com>
2023-08-15 21:28:44 -0300

    concertando scroll do modal

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ new Vue({
     async fazerFetchPastaProdutos(objProdutoId) {
       this.dadosProduto = await (await fetch(`./api/produtos/${objProdutoId}/dados.json`)).json()
     },
+    abrirOModal(objProdutoId) {
+      this.fazerFetchPastaProdutos(objProdutoId);
+      window.scrollTo({top: 0, behavior: "smooth"})
+    },
     fecharOModal({target, currentTarget}) {
       if(target === currentTarget) this.dadosProduto = false;
     },
